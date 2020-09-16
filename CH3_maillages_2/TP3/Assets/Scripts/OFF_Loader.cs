e26393ede1192e1016718897cca20f846eea8353
Niccolas_Calvet <nicolas.calvetgp@gmail.com>
2020-09-16 16:27:21 +0200

    Able to read data in header for OFF file

--- CH3_maillages_2/TP3/Assets/Scripts/OFF_Loader.cs
+++ CH3_maillages_2/TP3/Assets/Scripts/OFF_Loader.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class OFF_Loader : MonoBehaviour
@@ -7,14 +9,67 @@ public class OFF_Loader : MonoBehaviour
 
     public Material mat;
 
-    // Use this for initialization
+
     void Start()
+    {
+        ReadOFF("Assets/OFFMeshes/cube.off");
+    }
+
+    void ReadOFF(string fileName)
     {
         gameObject.AddComponent<MeshFilter>();
         gameObject.AddComponent<MeshRenderer>();
 
 
         /** Reading file **/
+        try
+        {
+            // Create an instance of StreamReader to read from a file.
+            // The using statement also closes the StreamReader.
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line = sr.ReadLine();
+                if (!line.Equals("OFF"))
+                {
+                    Debug.Log("Not a OFF file !");
+                    return;
+                }
+
+                line = sr.ReadLine();
+
+                uint[] fileData = Array.ConvertAll(line.Split(' '), uint.Parse);
+
+                if(fileData.Length != 3)
+                {
+                    Debug.Log("Cannot read file correctly (vertices, triangles & edge are incorrect)");
+                    return;
+                }
+
+                uint verticesCount = fileData[0];
+                uint trianglesCount = fileData[1];
+                uint edgeCount = fileData[2];
+
+                Debug.Log("Vertices : " + verticesCount);
+                Debug.Log("Triangles : " + trianglesCount);
+                Debug.Log("Edges : " + edgeCount);
+
+                // Read and display lines from the file until the end of
+                // the file is reached.
+                while ((line = sr.ReadLine()) != null)
+                {
+                    //Debug.Log(line);
+
+                    
+
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            // Let the user know what went wrong.
+            Debug.Log("The file could not be read:");
+            Debug.Log(e.Message);
+        }
 
         Vector3[] vertices = new Vector3[3];
         int[] triangles = new int[3];
