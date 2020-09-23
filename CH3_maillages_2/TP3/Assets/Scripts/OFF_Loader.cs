d8bf0d1248b65d7f64909407f166628e250a2d7e
Niccolas_Calvet <nicolas.calvetgp@gmail.com>
2020-09-23 15:39:16 +0200

    TP3 : Q5 computing triangles then vertices normals

--- CH3_maillages_2/TP3/Assets/Scripts/OFF_Loader.cs
+++ CH3_maillages_2/TP3/Assets/Scripts/OFF_Loader.cs
@@ -14,6 +14,8 @@ public class OFF_Loader : MonoBehaviour
     // Filename to read
     public string fileName;
 
+    // Using unity calculate normals OR my method
+    public bool unityNormals;
 
 
     /** Private data **/
@@ -42,6 +44,7 @@ public class OFF_Loader : MonoBehaviour
             ReadOFF("Assets/OFFMeshes/" + fileName);
             oldFileName = fileName;
         }
+
     }
 
     private void resetValues() {
@@ -85,126 +88,177 @@ public class OFF_Loader : MonoBehaviour
         Mesh msh = new Mesh();
 
         /** Reading file **/
-        try
+        // Opening file
+        using (StreamReader sr = new StreamReader(path))
         {
-            // Opening file
-            using (StreamReader sr = new StreamReader(path))
+
+            // Read first line "OFF"
+            string line = sr.ReadLine();
+            if (!line.Equals("OFF"))
             {
-
-                // Read first line "OFF"
-                string line = sr.ReadLine();
-                if (!line.Equals("OFF"))
-                {
-                    Debug.Log("Not a OFF file !");
-                    return;
-                }
+                Debug.Log("Not a OFF file !");
+                return;
+            }
 
 
-                // Read second lines : file data
+            // Read second lines : file data
+            line = sr.ReadLine();
+            int[] fileData = Array.ConvertAll(line.Split(' '), int.Parse);
+            if (fileData.Length != 3)
+            {
+                Debug.Log("Cannot read file correctly (wrong parameters number)");
+                return;
+            }
+
+            int verticesCount = fileData[0];
+            int trianglesCount = fileData[1];
+            int edgeCount = fileData[2];
+
+            //Debug.Log("Vertices : " + verticesCount);
+            //Debug.Log("Triangles : " + trianglesCount);
+            //Debug.Log("Edges : " + edgeCount);
+
+
+            // Create arrays
+            Vector3[] vertices = new Vector3[verticesCount];
+            int[] triangles = new int[trianglesCount * 3];
+
+
+            // Read vertices
+            for (int v = 0; v < verticesCount; ++v)
+            {
                 line = sr.ReadLine();
-                int[] fileData = Array.ConvertAll(line.Split(' '), int.Parse);
-                if (fileData.Length != 3)
+                string[] lineSplit = line.Split(' ');
+                if (lineSplit.Length < 3)
                 {
-                    Debug.Log("Cannot read file correctly (wrong parameters number)");
+                    Debug.Log("Cannot read vertex " + v + " correctly (wrong coordinates number)");
                     return;
                 }
 
-                int verticesCount = fileData[0];
-                int trianglesCount = fileData[1];
-                int edgeCount = fileData[2];
+                NumberFormatInfo format = new NumberFormatInfo();
+                format.NumberDecimalSeparator = ".";
+                format.NegativeSign = "-";
 
-                //Debug.Log("Vertices : " + verticesCount);
-                //Debug.Log("Triangles : " + trianglesCount);
-                //Debug.Log("Edges : " + edgeCount);
+                vertices[v] = new Vector3();
+                float.TryParse(lineSplit[0], NumberStyles.Float, format, out vertices[v].x);
+                float.TryParse(lineSplit[1], NumberStyles.Float, format, out vertices[v].y);
+                float.TryParse(lineSplit[2], NumberStyles.Float, format, out vertices[v].z);
 
+                // Sum of points for further centering
+                gravityCenterPoint += vertices[v];
 
-                // Create arrays
-                Vector3[] vertices = new Vector3[verticesCount];
-                int[] triangles = new int[trianglesCount * 3];
-
-
-                // Read vertices
-                for (int v = 0; v < verticesCount; ++v)
-                {
-                    line = sr.ReadLine();
-                    string[] lineSplit = line.Split(' ');
-                    if (lineSplit.Length < 3)
-                    {
-                        Debug.Log("Cannot read vertex " + v + " correctly (wrong coordinates number)");
-                        return;
-                    }
-
-                    NumberFormatInfo format = new NumberFormatInfo();
-                    format.NumberDecimalSeparator = ".";
-                    format.NegativeSign = "-";
-
-                    vertices[v] = new Vector3();
-                    float.TryParse(lineSplit[0], NumberStyles.Float, format, out vertices[v].x);
-                    float.TryParse(lineSplit[1], NumberStyles.Float, format, out vertices[v].y);
-                    float.TryParse(lineSplit[2], NumberStyles.Float, format, out vertices[v].z);
-
-                    // Sum of points for further centering
-                    gravityCenterPoint += vertices[v];
-
-                    // Getting the furthest point
-                    if (vertices[v].sqrMagnitude > squaredMagnitudePoint) {
-                        squaredMagnitudePoint = vertices[v].sqrMagnitude;
-                    }
-
+                // Getting the furthest point
+                if (vertices[v].sqrMagnitude > squaredMagnitudePoint) {
+                    squaredMagnitudePoint = vertices[v].sqrMagnitude;
                 }
-
-                // Getting real coordinates of gravity center point
-                gravityCenterPoint /= verticesCount;
-
-                // Centering the mesh around Vector3.zero
-                centeringMesh(ref vertices);
-
-                //Normlaizing mesh in range [-1;1]
-                normalizeMesh(ref vertices);
-
-
-                // Read triangles
-                for (int t = 0; t < trianglesCount * 3; t += 3)
-                {
-                    line = sr.ReadLine();
-                    string[] lineSplit = line.Split(' ');
-
-                    if (lineSplit.Length < 4)
-                    {
-                        Debug.Log("Cannot read triangle " + t + " correctly (wrong index number)");
-                        return;
-                    }
-
-                    int indexNumber;
-                    int.TryParse(lineSplit[0], out indexNumber);
-
-                    if (indexNumber != 3)
-                    {
-                        Debug.Log("Index numbers triangle " + t + " incorrect");
-                        return;
-                    }
-
-                    int.TryParse(lineSplit[1], out triangles[t]);
-                    int.TryParse(lineSplit[2], out triangles[t + 1]);
-                    int.TryParse(lineSplit[3], out triangles[t + 2]);
-                }
-
-                msh.vertices = vertices;
-                msh.triangles = triangles;
 
             }
-        }
-        catch (Exception e)
-        {
-            // Let the user know what went wrong.
-            Debug.Log("The file could not be read:");
-            Debug.Log(e.Message);
+
+            // Getting real coordinates of gravity center point
+            gravityCenterPoint /= verticesCount;
+
+            // Centering the mesh around Vector3.zero
+            centeringMesh(ref vertices);
+
+            //Normlaizing mesh in range [-1;1]
+            normalizeMesh(ref vertices);
+
+
+            // Read triangles
+            for (int t = 0; t < trianglesCount * 3; t += 3)
+            {
+                line = sr.ReadLine();
+                string[] lineSplit = line.Split(' ');
+
+                if (lineSplit.Length < 4)
+                {
+                    Debug.Log("Cannot read triangle " + t + " correctly (wrong index number)");
+                    return;
+                }
+
+                int indexNumber;
+                int.TryParse(lineSplit[0], out indexNumber);
+
+                if (indexNumber != 3)
+                {
+                    Debug.Log("Index numbers triangle " + t + " incorrect");
+                    return;
+                }
+
+                int.TryParse(lineSplit[1], out triangles[t]);
+                int.TryParse(lineSplit[2], out triangles[t + 1]);
+                int.TryParse(lineSplit[3], out triangles[t + 2]);
+            }
+
+            msh.vertices = vertices;
+            msh.triangles = triangles;
+
+            computeNormals(ref msh);
+
         }
 
 
         // Remplissage du Mesh et ajout du matériel
         gameObject.GetComponent<MeshFilter>().mesh = msh;          
         gameObject.GetComponent<MeshRenderer>().material = mat;
+    }
+
+    private void computeNormals(ref Mesh msh) {
+
+        Vector3[] normmals = new Vector3[msh.vertexCount];
+
+        // Triangle normals
+        Vector3[] triangleNormals = new Vector3[msh.triangles.Length / 3]; // Divided by 3 to get the real number of triangles, otherwise it represents the number of indices
+        
+        // List of triangles linked to vertices
+        List<int>[] verticesTriangle = new List<int>[msh.vertexCount];
+
+        for (int i = 0; i < verticesTriangle.Length; i++) {
+            verticesTriangle[i] = new List<int>();
+        }
+
+
+        /** Compute triangles normals **/
+        // Simple cross product
+        for (int t = 0; t < triangleNormals.Length; ++t) {
+
+            // Triangle index in Mesh object
+            int triangleIndex = t * 3;
+
+            // Retrieving points
+            Vector3 A = msh.vertices[ msh.triangles[triangleIndex] ];
+            Vector3 B = msh.vertices[ msh.triangles[triangleIndex+1] ];
+            Vector3 C = msh.vertices[ msh.triangles[triangleIndex+2] ];
+
+            // Computing vectors
+            Vector3 AB = B - A;
+            Vector3 AC = C - A;
+
+            // Computing triangle normal
+            triangleNormals[t] = Vector3.Cross(AB, AC);
+                
+            // Adding triangle to vertex lists
+            verticesTriangle[ msh.triangles[triangleIndex]   ].Add(t);
+            verticesTriangle[ msh.triangles[triangleIndex+1] ].Add(t);
+            verticesTriangle[ msh.triangles[triangleIndex+2] ].Add(t);
+
+        }
+
+        /** Compute vertices normals **/
+        // Simple average of connected triangle normals
+        for (int v = 0; v < verticesTriangle.Length; v++) {
+
+            normmals[v] = Vector3.zero;
+
+            for (int t = 0; t < verticesTriangle[v].Count; t++) {
+                normmals[v] += triangleNormals[ verticesTriangle[v][t] ];
+            }
+
+            normmals[v] /= verticesTriangle[v].Count;
+        }
+
+        msh.SetNormals(normmals);
+
     }
 
     // Taking a boolean parameter to print detailed vertices and triangles
